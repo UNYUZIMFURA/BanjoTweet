4f37de204bcdf040054e476ed143ba32a03c33b4
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-08 16:40:38 +0200

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,13 @@ import Upload from "@/components/upload/Upload";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col sm:flex-row">
+    <main className="flex min-h-screen w-screen min-[600px]:justify-center flex-col sm:flex-row">
       <SideBar />
       <div className="flex flex-col w-full border max-w-[40rem]">
         <Header />
         <Upload />
       </div>
-      <div className="hidden md:flex w-[30rem]"></div>
+      <div className="hidden md:flex w-[20rem] border"></div>
     </main>
   );
 }
