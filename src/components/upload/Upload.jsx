5c45b7735ff02cf0ebeec37c7de0a38f875a0a74
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-09 11:52:48 +0200

    ...

--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -9,10 +9,10 @@ import {
 
 const Upload = () => {
   return (
-    <div className="hidden px-4 gap-4 py-6 sm:flex items-center border-b border-custom">
+    <div className="hidden px-4 gap-4 pt-3 pb-1 sm:flex items-center border-b border-custom">
       <div className="flex items-start gap-4">
         <div className="rounded-full object-cover h-[40px] w-[40px] border"></div>
-        <div className="flex flex-col gap-[4rem]">
+        <div className="flex flex-col gap-[6.3rem] py-1">
           <span className="text-skin-secondary text-[20px]">
             What is happening?!
           </span>
