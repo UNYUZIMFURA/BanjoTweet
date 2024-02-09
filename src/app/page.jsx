2ede5c9ef4a5a1ac9a3f62f746f2d25ea2a7ac38
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-09 14:57:47 +0200

    Style: Pixel perfection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <main className="flex min-h-screen w-screen min-[690px]:justify-center flex-col sm:flex-row">
       <SideBar />
-      <div className="flex flex-col w-full max-w-[39rem]">
+      <div className="flex flex-col w-full max-w-[38rem]">
         <div className="flex flex-col w-full border-r border-b border-custom">
           <Header />
           <Upload />
         </div>
         <div className="h-full w-full hidden"></div>
       </div>
-      <div className="hidden md:flex w-[20rem] border-2 ml-4"></div>
+      <div className="hidden md:flex w-[18rem] border-2 ml-5"></div>
     </main>
   );
 }
