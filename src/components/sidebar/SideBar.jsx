6a028bbaeed17c5f0576c1bd3423d0ff4e33a24e
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-11 18:20:54 +0200

    Remove: Perfoming a code cleanup

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -13,52 +13,67 @@ import {
   CreateSvg,
 } from "@/svgs";
 
+const svgs = [
+  {
+    svg: HomeSvg ,
+    text: "Home",
+  },
+  {
+    svg: SearchSvg,
+    text: "Explore",
+  },
+  {
+    svg: GrokSvg,
+    text: "Grok",
+  },
+  {
+    svg: NotifySvg,
+    text: "Notifications",
+  },
+  {
+    svg: MessageSvg,
+    text: "Messages",
+  },
+  {
+    svg: ListSvg,
+    text: "Lists",
+  },
+  {
+    svg: BookMarkSvg,
+    text: "Bookmarks",
+  },
+  {
+    svg: CommunitySvg,
+    text: "Communities",
+  },
+  {
+    svg: ProfileSvg,
+    text: "Profile",
+  },
+  {
+    svg: MoreSvg,
+    text: "More",
+  },
+];
+
 const SideBar = () => {
   return (
     <div className="sm:gap-8 fixed bottom-0 w-full flex items-center justify-between px-4 py-3 sm:static sm:flex-col sm:w-[4.5rem] xl:w-[16.4rem] sm:border-r sm:border-custom sm:py-5 xl:items-start text-white">
       <div className="hidden sm:flex xl:px-">
         <LogoSvg />
       </div>
-      <div className="xl:flex items-center justify-center gap-4 xl:p-0">
-        <HomeSvg />
-        <span className="hidden xl:block text-[20px] font-bold">Home</span>
-      </div>
-      <div className="xl:flex items-center justify-center gap-4 xl:p-0">
-        <SearchSvg />
-        <span className="hidden xl:block text-[20px]">Explore</span>
-      </div>
-      <div className="xl:flex items-center justify-center gap-4 xl:p-0">
-        <GrokSvg />
-        <span className="hidden xl:block text-[20px]">Grok</span>
-      </div>
-      <div className="xl:flex items-center justify-center gap-4 xl:p-0">
-        <NotifySvg />
-        <span className="hidden xl:block text-[20px]">Notifications</span>
-      </div>
-      <div className="sm:flex items-center justify-center gap-4 xl:p-0">
-        <MessageSvg />
-        <span className="hidden xl:block text-[20px]">Messages</span>
-      </div>
-      <div className="sm:flex hidden items-center justify-center gap-4 xl:p-0">
-        <ListSvg />
-        <span className="hidden xl:block text-[20px]">Lists</span>
-      </div>
-      <div className="sm:flex hidden items-center justify-center gap-4 xl:p-0">
-        <BookMarkSvg />
-        <span className="hidden xl:block text-[20px]">Bookmarks</span>
-      </div>
-      <div className="sm:flex hidden items-center justify-center gap-4 xl:p-0">
-        <CommunitySvg />
-        <span className="hidden xl:block text-[20px]">Communities</span>
-      </div>
-      <div className="sm:flex hidden items-center justify-center gap-4 xl:p-0">
-        <ProfileSvg />
-        <span className="hidden xl:block text-[20px]">Profile</span>
-      </div>
-      <div className="sm:flex hidden items-center justify-center gap-4 xl:p-0">
-        <MoreSvg />
-        <span className="hidden xl:block text-[20px]">More</span>
-      </div>
+      {svgs.map((item, index) => (
+        <div
+          key={index}
+          className="xl:flex items-center justify-center gap-4 xl:p-0"
+        >
+          
+        <item.svg />
+          <span className="hidden xl:block text-[20px]">
+            {item.text}
+          </span>
+        </div>
+      ))}
       <div className="flex-col items-center gap-14 hidden sm:flex">
         <div className="h-[52px] w-[52px] rounded-full bg-custom flex items-center justify-center">
           <CreateSvg />
