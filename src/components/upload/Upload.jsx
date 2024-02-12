e76351ede3ab7220c1f7504081f0cee268a34740
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-12 20:29:18 +0200

    Add: Restructuring elements

--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -9,24 +9,25 @@ import {
 
 const Upload = () => {
   return (
-    <div className="text-white hidden gap-4 pt-4 pb-1 sm:flex flex-col items-center">
-      <div className="flex justify-around w-full">
-        <div className="rounded-full object-cover h-[40px] w-[40px] border"></div>
+    <div className="text-white hidden gap-4 pt-4 pb-1 sm:flex p-4">
+      <div className="rounded-full object-cover h-[40px] w-[40px] border"></div>
+      <div className="w-[90%] flex flex-col">
         <textarea
-          className="resize-none h-[120px] w-[80%] outline-none bg-black placeholder:text-skin-secondary placeholder:text-[20px]"
+          className="resize-none h-[100px] pt-1 outline-none bg-black placeholder:text-skin-secondary placeholder:text-[20px]"
           placeholder="What is happening?!"
         ></textarea>
-      </div>
-      <div className="flex w-full pl-20 justify-between pr-4">
-        <div className="flex gap-4">
-          <GallerySvg />
-          <EmojisSvg />
-          <GifSvg />
-          <PollSvg />
-          <ScheduleSvg />
-        </div>
-        <div className="flex items-center justify-center py-2 text-[0.9rem] font-black w-[4.8rem] rounded-full bg-custom opacity-50">
-          Post
+
+        <div className="flex w-full justify-between">
+          <div className="flex gap-4">
+            <GallerySvg />
+            <EmojisSvg />
+            <GifSvg />
+            <PollSvg />
+            <ScheduleSvg />
+          </div>
+          <div className="flex items-center justify-center py-2 text-[0.9rem] font-black w-[4.8rem] rounded-full bg-custom opacity-50">
+            Post
+          </div>
         </div>
       </div>
     </div>
