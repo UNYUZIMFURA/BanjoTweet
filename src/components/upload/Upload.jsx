a6f2a3dd68cac5fc9fbc79d9057fedc8c80dc713
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-12 14:47:45 +0200

    Add: Rebuilding components for perfect UI

--- src/components/upload/Upload.jsx
+++ src/components/upload/Upload.jsx
@@ -9,23 +9,22 @@ import {
 
 const Upload = () => {
   return (
-    <div className="hidden px-4 gap-4 pt-3 pb-1 sm:flex items-end">
-      <div className="flex items-start gap-4 w-[90%]">
+    <div className="text-white hidden gap-4 pt-3 pb-1 sm:flex flex-col items-center">
+      <div className="flex justify-around w-full bg-slat-200">
         <div className="rounded-full object-cover h-[40px] w-[40px] border"></div>
-        <div className="flex flex-col gap-[6.3rem] py-1">
-          <span className="text-skin-secondary text-[20px]">
-            What is happening?!
-          </span>
-          <div className="flex gap-4">
-            <GallerySvg />
-            <EmojisSvg />
-            <GifSvg />
-            <PollSvg />
-            <ScheduleSvg />
-          </div>
-        </div>
+        <textarea
+          className="resize-none h-[168px] w-[80%] outline-none bg-black placeholder:text-skin-secondary placeholder:text-[20px]"
+          placeholder="What is happening?!"
+        ></textarea>
       </div>
-      <div className="flex items-center justify-center py-2 text-[0.9rem] font-black w-[4.8rem] text-white rounded-full bg-custom opacity-50">
+      <div className="flex gap-4">
+        <GallerySvg />
+        <EmojisSvg />
+        <GifSvg />
+        <PollSvg />
+        <ScheduleSvg />
+      </div>
+      <div className="flex items-center justify-center py-2 text-[0.9rem] font-black w-[4.8rem] rounded-full bg-custom opacity-50">
         Post
       </div>
     </div>
