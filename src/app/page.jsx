6d0f4cfb7cc19e811112cea9f4a8e5576217b928
UNYUZIMFURA <banjoflylive@gmail.com>
2024-02-14 13:10:55 +0200

    ...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,8 @@ import Recommended from "@/components/recommended/Recommended";
 
 export default function Home() {
   return (
-    <main className="flex h-screen borde w-screen min-[690px]:justify-center flex-col sm:flex-row">
-      <SideBar />
+    <main className="flex h-screen w-screen min-[690px]:justify-center flex-col sm:flex-row">
+      <SideBar /> 
       <div className="flex flex-col w-full max-w-[38rem]">
         <div className="flex flex-col w-full border-r border-b border-custom">
           <Header />
