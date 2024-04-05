8e476a5909330eac8d184edf9153cb0cc84e19e4
UNYUZIMFURA <banjoflylive@gmail.com>
2024-04-05 15:17:28 +0200

    ...

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,10 +6,10 @@ import Posts from "@/components/posts/Posts";
 
 export default function Home() {
   return (
-    <main className="flex h-screen w-screen min-[690px]:justify-center flex-col sm:flex-row max-w-[80rem] items-center mx-auto">
-      <div className="flex h-full w-full">
+    <main className="flex h-screen w-screen flex-col sm:flex-row max-w-[80rem] items-center mx-auto">
+      <div className="min-[690px]:justify-center flex h-full w-full min-[690px]:mx-auto">
         <SideBar />
-        <div className="sm:ml-[4.5rem] xl:ml-[16.5rem] w-full flex flex-col max-w-[38rem]">
+        <div className="sm:ml-[4.5rem] min-[690px]:ml-[6rem] xl:ml-[16.5rem] w-full flex flex-col max-w-[38rem]">
           <div className="flex flex-col w-full border-r border-b border-custom">
             <Header />
             <Upload />
