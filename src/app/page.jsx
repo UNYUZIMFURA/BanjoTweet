477bf18d52a0d6d0ee3bc4fda79d5c8489d22ae1
UNYUZIMFURA <banjoflylive@gmail.com>
2024-04-26 18:00:05 +0200

    Style: Still dealing with positioning properly the fixed sidebar

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -7,14 +7,16 @@ import Posts from "@/components/posts/Posts";
 export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col sm:flex-row max-w-[80rem] items-center mx-auto">
-      <div className="min-[690px]:justify-center flex h-full w-full min-[690px]:mx-auto">
-        <SideBar />
-        <div className="sm:ml-[4.5rem] min-[690px]:ml-[6rem] xl:ml-[16.5rem] w-full flex flex-col max-w-[38rem]">
-          <div className="flex flex-col w-full border-r border-b border-custom">
-            <Header />
-            <Upload />
+      <div className="flex justify-center h-full w-full mx-auto">
+        <div className="w-full min-[690px]:w-[calc(100%-4.5rem)] flex justify-start">
+          <SideBar />
+          <div className="sm:ml-[4.5rem] min-[690px]:ml-[6rem] xl:ml-[16.5rem] w-full flex flex-col max-w-[38rem]">
+            <div className="flex flex-col w-full border-r border-b border-custom">
+              <Header />
+              <Upload />
+            </div>
+            <Posts />
           </div>
-          <Posts />
         </div>
         <Recommended />
       </div>
