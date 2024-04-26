83d1a2ee81543fff8d53d7e10011e6b1106658b1
UNYUZIMFURA <banjoflylive@gmail.com>
2024-04-26 20:03:39 +0200

    Style: Finished Positioning the Sidebar Properly

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <main className="flex h-screen w-screen flex-col sm:flex-row max-w-[80rem] items-center mx-auto">
       <div className="flex justify-center h-full w-full mx-auto">
-        <div className="w-full min-[690px]:w-[calc(100%-4.5rem)] flex justify-start">
+        <div className="w-full min-[690px]:w-[calc(100%-4.5rem)] min-[760px]:w-[42.5rem] flex justify-start min-[1003px]:w-[54.5rem] min-[1093px]:w-[60rem">
           <SideBar />
-          <div className="sm:ml-[4.5rem] min-[690px]:ml-[6rem] xl:ml-[16.5rem] w-full flex flex-col max-w-[38rem]">
+          <div className="sm:ml-[4.5rem] xl:ml-[16.5rem] w-full flex flex-col max-w-[38rem]">
             <div className="flex flex-col w-full border-r border-b border-custom">
               <Header />
               <Upload />
