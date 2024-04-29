fcac14a29ebe299793a39ca66aa868011d02cde4
UNYUZIMFURA <banjoflylive@gmail.com>
2024-04-29 15:29:50 +0200

    Style: Adjusting properly the layout of the Sidebar

--- custom.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.svg" {
-    import React from "react"
-    export const ReactComponent: React.FunctionComponent<
-        React.SVGProps<SVGSVGElement>
-    >
-}
--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,105 +1,118 @@
 import {
-  LogoSvg,
-  LogoSmallSvg,
-  HomeSvg,
-  SearchSvg,
-  GrokSvg,
-  NotifySvg,
-  MessageSvg,
-  ListSvg,
-  BookMarkSvg,
-  CommunitySvg,
-  ProfileSvg,
-  MoreSvg,
-  CreateSvg,
-} from "@/utils/svgs";
+    LogoSvg,
+    LogoSmallSvg,
+    HomeSvg,
+    SearchSvg,
+    GrokSvg,
+    NotifySvg,
+    MessageSvg,
+    ListSvg,
+    BookMarkSvg,
+    CommunitySvg,
+    ProfileSvg,
+    MoreSvg,
+    CreateSvg,
+} from "@/utils/svgs"
 
 const svgs = [
-  {
-    svg: HomeSvg,
-    text: "Home",
-  },
-  {
-    svg: SearchSvg,
-    text: "Explore",
-  },
-  {
-    svg: GrokSvg,
-    text: "Grok",
-  },
-  {
-    svg: NotifySvg,
-    text: "Notifications",
-  },
-  {
-    svg: MessageSvg,
-    text: "Messages",
-  },
+    {
+        svg: HomeSvg,
+        text: "Home",
+    },
+    {
+        svg: SearchSvg,
+        text: "Explore",
+    },
+    {
+        svg: GrokSvg,
+        text: "Grok",
+    },
+    {
+        svg: NotifySvg,
+        text: "Notifications",
+    },
+    {
+        svg: MessageSvg,
+        text: "Messages",
+    },
 
-  {
-    svg: ListSvg,
-    text: "Lists",
-  },
-  {
-    svg: BookMarkSvg,
-    text: "Bookmarks",
-  },
-  {
-    svg: CommunitySvg,
-    text: "Communities",
-  },
-  {
-    svg: LogoSmallSvg,
-    text: "Premium",
-  },
-  {
-    svg: ProfileSvg,
-    text: "Profile",
-  },
-  {
-    svg: MoreSvg,
-    text: "More",
-  },
-];
+    {
+        svg: ListSvg,
+        text: "Lists",
+    },
+    {
+        svg: BookMarkSvg,
+        text: "Bookmarks",
+    },
+    {
+        svg: CommunitySvg,
+        text: "Communities",
+    },
+    {
+        svg: LogoSmallSvg,
+        text: "Premium",
+    },
+    {
+        svg: ProfileSvg,
+        text: "Profile",
+    },
+    {
+        svg: MoreSvg,
+        text: "More",
+    },
+]
 
 const SideBar = () => {
-  return (
-    <div className="bg-black z-10 sm:justify-normal sm:gap-[1.97rem] fixed bottom-0 w-full sm:h-full flex items-center justify-between px-4 py-3 sm:flex-col sm:w-[4.5rem] xl:w-[16.5rem] sm:border-r sm:border-custom sm:py-3 xl:items-start text-white">
-      <div className="hidden sm:flex">
-        <LogoSvg />
-      </div>
-      {svgs.map((item, index) =>
-        item.text === "Lists" ||
-        item.text === "Bookmarks" ||
-        item.text === "Communities" ||
-        item.text === "Premium" ||
-        item.text === "Profile" ||
-        item.text === "More" ? (
-          <div
-            key={index}
-            className="sm:flex hidden items-center justify-center gap-4"
-          >
-            <item.svg />
-            <span className="hidden xl:block text-[20px]">{item.text}</span>
-          </div>
-        ) : (
-          <div
-            key={index}
-            className="xl:flex items-center justify-center gap-4"
-          >
-            <item.svg />
-            <span className="hidden xl:block text-[20px]">{item.text}</span>
-          </div>
-        )
-      )}
-      <div className="flex-col items-center gap-[3.4rem] hidden sm:flex">
-        <div className="h-[52px] w-[52px] rounded-full bg-custom flex items-center justify-center">
-          <CreateSvg />
+    return (
+        <div className="fixed bottom-0 z-10 flex w-full items-center justify-between bg-black py-3 text-white sm:h-screen sm:w-[4.5rem] sm:flex-col sm:border-r sm:border-custom sm:py-3 xl:w-[16.5rem] xl:items-start">
+            <div className="flex w-full items-center justify-between px-4 sm:flex-col sm:gap-[2rem] xl:items-start xl:gap-5">
+                <div className="hidden sm:flex">
+                    <LogoSvg />
+                </div>
+                {svgs.map((item, index) =>
+                    item.text === "Lists" ||
+                    item.text === "Profile" ||
+                    item.text === "Premium" ||
+                    item.text === "More" ? (
+                        <div
+                            key={index}
+                            className="hidden items-center justify-center gap-4 sm:flex"
+                        >
+                            <item.svg />
+                            <span className="hidden text-[20px] xl:block">
+                                {item.text}
+                            </span>
+                        </div>
+                    ) : item.text === "Bookmarks" ||
+                      item.text === "Communities" ? (
+                        <div
+                            key={index}
+                            className="hidden items-center justify-center gap-4 xl:flex"
+                        >
+                            <item.svg />
+                            <span className="hidden text-[20px] xl:block">
+                                {item.text}
+                            </span>
+                        </div>
+                    ) : (
+                        <div
+                            key={index}
+                            className="items-center justify-center gap-4 xl:flex"
+                        >
+                            <item.svg />
+                            <span className="hidden text-[20px] xl:block">
+                                {item.text}
+                            </span>
+                        </div>
+                    ),
+                )}
+                <div className="hidden h-[52px] w-[52px] items-center justify-center rounded-full bg-custom sm:flex">
+                    <CreateSvg />
+                </div>
+            </div>
+            <div className="hidden h-[40px] w-[40px] rounded-full border object-cover sm:flex xl:ml-4"></div>
         </div>
-        <div className="rounded-full object-cover h-[40px] w-[40px] border"></div>
-      </div>
-    </div>
-  );
-};
+    )
+}
 
-export default SideBar;
+export default SideBar
