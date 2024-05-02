67f90590d73d541f5f42965a6e43acbbdd1b7bba
UNYUZIMFURA <banjoflylive@gmail.com>
2024-05-02 11:48:33 +0200

    Fix: Fix colour not changing on click to Post Icons

--- src/components/posts/Post.tsx
+++ src/components/posts/Post.tsx
@@ -158,7 +158,7 @@ const Post: React.FC<PostProps> = ({
                             viewBox="0 0 24 24"
                             aria-hidden="true"
                             width="20"
-                            className={`fill-[rgb(113,118,123)] transition ease-in-out ${bookmarked ? "fill-[#1D9BF0]" : ""}`}
+                            className={`transition ease-in-out ${bookmarked ? "fill-[#1D9BF0]" : "fill-[rgb(113,118,123)]"}`}
                         >
                             <g>
                                 <path d="M4 4.5C4 3.12 5.119 2 6.5 2h11C18.881 2 20 3.12 20 4.5v18.44l-8-5.71-8 5.71V4.5zM6.5 4c-.276 0-.5.22-.5.5v14.56l6-4.29 6 4.29V4.5c0-.28-.224-.5-.5-.5h-11z"></path>
@@ -173,7 +173,7 @@ const Post: React.FC<PostProps> = ({
                             viewBox="0 0 24 24"
                             aria-hidden="true"
                             width="20"
-                            className={`fill-[rgb(113,118,123)] transition ease-in-out ${uploaded ? "fill-[#1D9BF0]" : ""} `}
+                            className={`transition ease-in-out ${uploaded ? "fill-[#1D9BF0]" : "fill-[rgb(113,118,123)]"} `}
                         >
                             <g>
                                 <path d="M12 2.59l5.7 5.7-1.41 1.42L13 6.41V16h-2V6.41l-3.3 3.3-1.41-1.42L12 2.59zM21 15l-.02 3.51c0 1.38-1.12 2.49-2.5 2.49H5.5C4.11 21 3 19.88 3 18.5V15h2v3.5c0 .28.22.5.5.5h12.98c.28 0 .5-.22.5-.5L19 15h2z"></path>
